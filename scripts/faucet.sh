de6dbca822ed9c6d485c21d2d5ce834ec33b0989
fixorium <fixercoin.token@gmail.com>
2025-07-20 19:44:50 +0500

    Update faucet.sh

--- scripts/faucet.sh
+++ scripts/faucet.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+fixoriumd tx bank send YOUR_FAUCET_WALLET $1 1000fixorium \
+  --chain-id fixorium-devnet \
+  --node http://localhost:26657 \
+  --keyring-backend test \
+  --broadcast-mode block \
+  --yes
 
