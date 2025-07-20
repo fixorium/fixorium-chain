6a178c237ffb7be4b0b17fbd346150bd9aec32c8
fixorium <fixercoin.token@gmail.com>
2025-07-20 19:49:05 +0500

    Update app.go

--- app/app.go
+++ app/app.go
@@ -1,1 +1,15 @@
+package app
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/simapp"
+)
+
+func NewFixoriumApp() *baseapp.BaseApp {
+	app := simapp.Setup(false) // start from default app
+	app.Name = "Fixorium"
+	sdk.DefaultBondDenom = "fixorium" // token name
+	return app
+}
 
